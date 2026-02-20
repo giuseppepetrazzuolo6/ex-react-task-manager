c05637bed72de2d6a2fe9da4b03dde741faec07b
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-20 12:30:17 +0100

    milestone-3
    created the task table and optimized performance with react.memo

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,8 +1,30 @@
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalContext"
+import TaskRaw from "../components/TaskRaw"
+
 export default function TaskList() {
+    const { tasks } = useContext(GlobalContext)
 
     return (
-        <div>
-            <h1>Qui ci sono le tasks...</h1>
+        <div className="container">
+            <h3>Qui ci sono le tasks...</h3>
+            <table className="task-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        tasks.map(task => (
+                            <TaskRaw key={task.id} task={task} />
+                        ))
+                    }
+
+                </tbody>
+            </table>
         </div>
     )
 }
