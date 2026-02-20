9626da89e150d762664aa316e182fcbfc87c2d71
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-20 13:01:08 +0100

    some fix to form

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -3,18 +3,18 @@ export default function AddTask() {
     return (
         <>
             <div className="container">
-                <form>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Task name</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" />
+                <form className="mt-3">
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlInput1">Task name</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" />
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Description task</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <div className="form-group mt-2">
+                        <label htmlFor="exampleFormControlTextarea1">Description task</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Statu</label>
-                        <select class="form-control" id="exampleFormControlSelect1">
+                    <div className="form-group mt-2">
+                        <label htmlFor="exampleFormControlSelect1">Status</label>
+                        <select className="form-control" id="exampleFormControlSelect1">
                             <option>To do</option>
                             <option>Doing</option>
                             <option>Done</option>
