bf6007c0f84f48db20f60a3bcc66202465fa9f50
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-20 12:56:47 +0100

    added form for new task

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,6 +1,27 @@
 export default function AddTask() {
 
     return (
-        <h1>Qui puoi aggiungere le task</h1>
+        <>
+            <div className="container">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Task name</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" />
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Description task</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Statu</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>To do</option>
+                            <option>Doing</option>
+                            <option>Done</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
